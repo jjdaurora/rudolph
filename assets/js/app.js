46a42a9168e73dcfa8b16f2ba7086a1255da071d
JJ D'Aurora <jjdaurora@gmail.com>
2017-12-29 03:34:45 -0500

    project complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,3 +30,64 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+var checkboxes = 0; 
+
+ $(document).ready(function() {
+    $('input[type="checkbox"]').click(function() {
+        
+        if($(this).prop("checked") == true) {
+            checkboxes++;
+            console.log(checkboxes);
+        };
+
+        if (checkboxes == 6) {
+            $('#message-area').html("That's right. I love so much about you!" + "<br/>" + "<a href='#year-ahead-section' class='js-scroll-trigger'>" + "<button>" + "Moving on...")
+        }
+
+    });
+});
+
+ (function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 0)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  //Custom addition for elements not on the DOM
+  $(document).on('click', 'a.js-scroll-trigger[href*="#"]:not([href="#"])', function(){
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 0)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
+
+})(jQuery); // End of use strict
